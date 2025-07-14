0d3fb9b2ad4632149270b8ce522628c8900c1fcc
yihuang <yi.codeplayer@gmail.com>
2025-07-14 17:41:54 +0800

    fix conditon

--- api/cosmos/evm/vm/v1/msg.go
+++ api/cosmos/evm/vm/v1/msg.go
@@ -24,7 +24,7 @@ func getSender(txData TxDataV2) (common.Address, error) {
 	chainID := txData.GetChainID()
 	// legacy tx returns `0` as chainID when EIP-155 is not used
 	// seee: DeriveChainID
-	if chainID.Sign() == 0 {
+	if chainID != nil && chainID.Sign() == 0 {
 		chainID = nil
 	}
 	signer := ethtypes.LatestSignerForChainID(chainID)
