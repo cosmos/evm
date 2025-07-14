6e2965648e15c03a0b91b311987c2dfb4e5669c1
yihuang <yi.codeplayer@gmail.com>
2025-07-14 17:40:58 +0800

    fix comment

--- api/cosmos/evm/vm/v1/msg.go
+++ api/cosmos/evm/vm/v1/msg.go
@@ -23,7 +23,7 @@ var supportedTxs = map[string]func() TxDataV2{
 func getSender(txData TxDataV2) (common.Address, error) {
 	chainID := txData.GetChainID()
 	// legacy tx returns `0` as chainID when EIP-155 is not used
-	// see: https://github.com/cosmos/evm/issues/280
+	// seee: DeriveChainID
 	if chainID.Sign() == 0 {
 		chainID = nil
 	}
