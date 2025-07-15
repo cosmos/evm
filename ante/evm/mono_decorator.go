8920dc3a82b6004a30f460f1bb57c895bb0db636
yihuang <yi.codeplayer@gmail.com>
2025-07-15 12:17:11 +0800

    feat: re-use tx validation rules from go-ethereum

--- ante/evm/mono_decorator.go
+++ ante/evm/mono_decorator.go
@@ -1,9 +1,11 @@
 package evm
 
 import (
+	"math"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/txpool"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 
 	anteinterfaces "github.com/cosmos/evm/ante/interfaces"
@@ -17,6 +19,12 @@ import (
 	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 )
+
+const ACCEPTED_TX_TYPE = 0 |
+	1<<ethtypes.LegacyTxType |
+	1<<ethtypes.AccessListTxType |
+	1<<ethtypes.DynamicFeeTxType |
+	1<<ethtypes.SetCodeTxType
 
 // MonoDecorator is a single decorator that handles all the prechecks for
 // ethereum transactions.
@@ -82,6 +90,23 @@ func (md MonoDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, ne
 		if err != nil {
 			return ctx, err
 		}
+		ethTx := ethMsg.AsTransaction()
+
+		// call go-ethereum transaction validation
+		header := ethtypes.Header{
+			GasLimit: ctx.BlockGasMeter().Limit(),
+			BaseFee:  decUtils.BaseFee,
+			Number:   big.NewInt(ctx.BlockHeight()),
+			Time:     uint64(ctx.BlockTime().Unix()),
+		}
+		if err := txpool.ValidateTransaction(ethTx, &header, decUtils.Signer, &txpool.ValidationOptions{
+			Config:  evmtypes.GetEthChainConfig(),
+			Accept:  ACCEPTED_TX_TYPE,
+			MaxSize: math.MaxUint64, // tx size is checked in cometbft
+			MinTip:  new(big.Int),
+		}); err != nil {
+			return ctx, err
+		}
 
 		feeAmt := txData.Fee()
 		gas := txData.GetGas()
@@ -126,6 +151,7 @@ func (md MonoDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, ne
 		// 5. signature verification
 		if err := SignatureVerification(
 			ethMsg,
+			ethTx,
 			decUtils.Signer,
 			decUtils.EvmParams.AllowUnprotectedTxs,
 		); err != nil {
