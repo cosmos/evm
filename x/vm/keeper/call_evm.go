2bbcca44a03f39de038fae4b1bbd92c597f08c68
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-07-21 11:44:22 +0530

    refactor: removed extra gasLimit param from derived tx methods

--- x/vm/keeper/call_evm.go
+++ x/vm/keeper/call_evm.go
@@ -117,7 +117,7 @@ func (k Keeper) DerivedEVMCall(
 	ctx sdk.Context,
 	abi abi.ABI,
 	from, contract common.Address,
-	value, gasLimit *big.Int,
+	value *big.Int,
 	commit, gasless bool,
 	method string,
 	args ...interface{},
@@ -130,7 +130,7 @@ func (k Keeper) DerivedEVMCall(
 		)
 	}
 
-	resp, err := k.DerivedEVMCallWithData(ctx, from, &contract, data, commit, gasless, value, gasLimit)
+	resp, err := k.DerivedEVMCallWithData(ctx, from, &contract, data, commit, gasless, value)
 	if err != nil {
 		return nil, errorsmod.Wrapf(err, "contract call failed: method '%s', contract '%s'", method, contract)
 	}
@@ -160,7 +160,7 @@ func (k Keeper) DerivedEVMCallWithData(
 	contract *common.Address,
 	data []byte,
 	commit, gasless bool,
-	value, gasLimit *big.Int,
+	value *big.Int,
 ) (*types.MsgEthereumTxResponse, error) {
 	nonce, err := k.accountKeeper.GetSequence(ctx, from.Bytes())
 	if err != nil {
@@ -168,7 +168,7 @@ func (k Keeper) DerivedEVMCallWithData(
 	}
 
 	gasCap := config.DefaultGasCap
-	if commit && gasLimit == nil {
+	if commit {
 		args, err := json.Marshal(types.TransactionArgs{
 			From: &from,
 			To:   contract,
@@ -186,9 +186,6 @@ func (k Keeper) DerivedEVMCallWithData(
 			return nil, err
 		}
 		gasCap = gasRes.Gas
-	}
-	if gasLimit != nil {
-		gasCap = gasLimit.Uint64()
 	}
 
 	msg := ethtypes.NewMessage(
