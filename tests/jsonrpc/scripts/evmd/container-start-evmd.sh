01b397f262c5eca12025352dd0524c0853f0b4a9
Haber <bluejet1104@gmail.com>
2025-08-14 02:21:20 +0900

    fix: github-ci

--- tests/jsonrpc/scripts/evmd/container-start-evmd.sh
+++ tests/jsonrpc/scripts/evmd/container-start-evmd.sh
@@ -70,28 +70,6 @@ evmd genesis gentx "$VAL_KEY" 1000000000000000000000atest --gas-prices "${BASEFE
 evmd genesis collect-gentxs --home "$CHAINDIR"
 evmd genesis validate-genesis --home "$CHAINDIR"
 
-# Configure timeout settings for faster block times (from local_node.sh)
-echo "🔧 Configuring consensus timeouts for faster block production..."
-if [[ "$OSTYPE" == "darwin"* ]]; then
-		sed -i '' 's/timeout_propose = "3s"/timeout_propose = "2s"/g' "$CONFIG_TOML"
-		sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "200ms"/g' "$CONFIG_TOML"
-		sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "500ms"/g' "$CONFIG_TOML"
-		sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "200ms"/g' "$CONFIG_TOML"
-		sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "500ms"/g' "$CONFIG_TOML"
-		sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "200ms"/g' "$CONFIG_TOML"
-		sed -i '' 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$CONFIG_TOML"
-		sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "5s"/g' "$CONFIG_TOML"
-	else
-		sed -i 's/timeout_propose = "3s"/timeout_propose = "2s"/g' "$CONFIG_TOML"
-		sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "200ms"/g' "$CONFIG_TOML"
-		sed -i 's/timeout_prevote = "1s"/timeout_prevote = "500ms"/g' "$CONFIG_TOML"
-		sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "200ms"/g' "$CONFIG_TOML"
-		sed -i 's/timeout_precommit = "1s"/timeout_precommit = "500ms"/g' "$CONFIG_TOML"
-		sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "200ms"/g' "$CONFIG_TOML"
-		sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$CONFIG_TOML"
-		sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "5s"/g' "$CONFIG_TOML"
-	fi
-
 echo "🚀 Starting evmd..."
 exec evmd start \
     --home "$CHAINDIR" \
