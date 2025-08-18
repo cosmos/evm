d98cc3b6276427172e6e31a6f09626633a5ceba1
Facundo <facundomedica@gmail.com>
2025-08-18 17:29:45 +0200

    feat: allow PostProcessingTx to run on failures and persist data

--- x/vm/keeper/state_transition.go
+++ x/vm/keeper/state_transition.go
@@ -205,53 +205,69 @@ func (k *Keeper) ApplyTransaction(ctx sdk.Context, tx *ethtypes.Transaction) (*t
 	ethLogs := types.LogsToEthereum(res.Logs)
 	_, bloomReceipt := k.initializeBloomFromLogs(ctx, ethLogs)
 
-	if !res.Failed() {
-		var contractAddr common.Address
-		if msg.To == nil {
-			contractAddr = crypto.CreateAddress(msg.From, msg.Nonce)
-		}
+	var contractAddr common.Address
+	if msg.To == nil {
+		contractAddr = crypto.CreateAddress(msg.From, msg.Nonce)
+	}
 
-		receipt := &ethtypes.Receipt{
-			Type:              tx.Type(),
-			PostState:         nil,
-			CumulativeGasUsed: calculateCumulativeGasFromEthResponse(ctx.GasMeter(), res),
-			Bloom:             bloomReceipt,
-			Logs:              ethLogs,
-			TxHash:            txConfig.TxHash,
-			ContractAddress:   contractAddr,
-			GasUsed:           res.GasUsed,
-			BlockHash:         txConfig.BlockHash,
-			BlockNumber:       big.NewInt(ctx.BlockHeight()),
-			TransactionIndex:  txConfig.TxIndex,
-		}
+	receipt := &ethtypes.Receipt{
+		Type:              tx.Type(),
+		PostState:         nil,
+		CumulativeGasUsed: calculateCumulativeGasFromEthResponse(ctx.GasMeter(), res),
+		Bloom:             bloomReceipt,
+		Logs:              ethLogs,
+		TxHash:            txConfig.TxHash,
+		ContractAddress:   contractAddr,
+		GasUsed:           res.GasUsed,
+		BlockHash:         txConfig.BlockHash,
+		BlockNumber:       big.NewInt(ctx.BlockHeight()),
+		TransactionIndex:  txConfig.TxIndex,
+	}
 
-		signerAddr, err := signer.Sender(tx)
-		if err != nil {
-			return nil, errorsmod.Wrap(err, "failed to extract sender address from ethereum transaction")
-		}
+	if res.Failed() {
+		receipt.Status = ethtypes.ReceiptStatusFailed
 
-		eventsLen := len(tmpCtx.EventManager().Events())
+		// If the tx failed we discard the old context and create a new one, so
+		// PostTxProcessing can persist data even if the tx fails.
+		tmpCtx, commitFn = ctx.CacheContext()
+	} else {
+		receipt.Status = ethtypes.ReceiptStatusSuccessful
+	}
 
-		// Note: PostTxProcessing hooks currently do not charge for gas
-		// and function similar to EndBlockers in abci, but for EVM transactions
-		if err = k.PostTxProcessing(tmpCtx, signerAddr, *msg, receipt); err != nil {
-			// If hooks returns an error, revert the whole tx.
-			res.VmError = errorsmod.Wrap(err, "failed to execute post transaction processing").Error()
-			k.Logger(ctx).Error("tx post processing failed", "error", err)
-			// If the tx failed in post processing hooks, we should clear all log-related data
-			// to match EVM behavior where transaction reverts clear all effects including logs
+	signerAddr, err := signer.Sender(tx)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "failed to extract sender address from ethereum transaction")
+	}
+
+	eventsLen := len(tmpCtx.EventManager().Events())
+
+	// Note: PostTxProcessing hooks currently do not charge for gas
+	// and function similar to EndBlockers in abci, but for EVM transactions.
+	// It will persist data even if the tx fails.
+	if err = k.PostTxProcessing(tmpCtx, signerAddr, *msg, receipt); err != nil {
+		// If hooks returns an error, revert the whole tx.
+		res.VmError = errorsmod.Wrap(err, "failed to execute post transaction processing").Error()
+		k.Logger(ctx).Error("tx post processing failed", "error", err)
+		// If the tx failed in post processing hooks, we should clear all log-related data
+		// to match EVM behavior where transaction reverts clear all effects including logs
+		res.Logs = nil
+		receipt.Logs = nil
+		receipt.Bloom = ethtypes.Bloom{} // Clear bloom filter
+	} else if commitFn != nil {
+		commitFn()
+
+		// Since the post-processing can alter the log, we need to update the result
+		if res.Failed() {
 			res.Logs = nil
 			receipt.Logs = nil
-			receipt.Bloom = ethtypes.Bloom{} // Clear bloom filter
-		} else if commitFn != nil {
-			commitFn()
-
-			// Since the post-processing can alter the log, we need to update the result
+			receipt.Bloom = ethtypes.Bloom{}
+		} else {
 			res.Logs = types.NewLogsFromEth(receipt.Logs)
-			events := tmpCtx.EventManager().Events()
-			if len(events) > eventsLen {
-				ctx.EventManager().EmitEvents(events[eventsLen:])
-			}
+		}
+
+		events := tmpCtx.EventManager().Events()
+		if len(events) > eventsLen {
+			ctx.EventManager().EmitEvents(events[eventsLen:])
 		}
 	}
 
