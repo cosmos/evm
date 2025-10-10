f3609932aa601c0bd8528f3bfc5cdbef99e298f4
mmsqe <tqd0800210105@gmail.com>
2025-10-10 08:24:02 +0800

    allow to generate both

--- evmd/cmd/evmd/cmd/root.go
+++ evmd/cmd/evmd/cmd/root.go
@@ -18,7 +18,7 @@ import (
 	dbm "github.com/cosmos/cosmos-db"
 	cosmosevmcmd "github.com/cosmos/evm/client"
 	evmdebug "github.com/cosmos/evm/client/debug"
-	cosmosevmkeyring "github.com/cosmos/evm/crypto/keyring"
+	"github.com/cosmos/evm/crypto/hd"
 	"github.com/cosmos/evm/evmd"
 	cosmosevmserver "github.com/cosmos/evm/server"
 	srvflags "github.com/cosmos/evm/server/flags"
@@ -85,7 +85,7 @@ func NewRootCmd() *cobra.Command {
 		WithHomeDir(config.MustGetDefaultNodeHome()).
 		WithViper(""). // In simapp, we don't use any prefix for env variables.
 		// Cosmos EVM specific setup
-		WithKeyringOptions(cosmosevmkeyring.Option()).
+		WithKeyringOptions(hd.EthSecp256k1Option()).
 		WithLedgerHasProtobuf(true)
 
 	rootCmd := &cobra.Command{
