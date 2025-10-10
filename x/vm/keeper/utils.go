4c37cf08f875ebab6e14bc8b42a1b62463e45ab7
Kyuhyeon Choi <bluejet1104@gmail.com>
2025-10-10 21:38:58 +0900

    chore: add comment

--- x/vm/keeper/utils.go
+++ x/vm/keeper/utils.go
@@ -8,6 +8,8 @@ import (
 )
 
 // IsContract determines if the given address is a smart contract.
+// It checks if the account has associated code and ensures that
+// the code is not a delegated contract (EIP-7702).
 func (k *Keeper) IsContract(ctx sdk.Context, addr common.Address) bool {
 	codeHash := k.GetCodeHash(ctx, addr)
 	code := k.GetCode(ctx, codeHash)
