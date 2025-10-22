45b28f31d9b98ad7957d5c533983ef7d1cc4efa9
srdtrk <srdtrk@hotmail.com>
2025-10-22 16:07:59 +0400

    imp: added evmKeeper

--- x/ibc/clients/keeper/keeper.go
+++ x/ibc/clients/keeper/keeper.go
@@ -18,6 +18,7 @@ type Keeper struct {
 	// authority is the address capable of executing a MsgUpdateParams and other authority-gated message.
 	authority sdk.AccAddress
 
+	evmKeeper    types.EVMKeeper
 	clientKeeper types.ClientKeeper
 
 	// state management
@@ -30,10 +31,16 @@ type Keeper struct {
 }
 
 // NewKeeper creates a new Keeper instance
-func NewKeeper(cdc codec.BinaryCodec, addressCodec address.Codec, storeService storetypes.KVStoreService, authority sdk.AccAddress, clientKeeper types.ClientKeeper) Keeper {
+func NewKeeper(cdc codec.BinaryCodec, addressCodec address.Codec, storeService storetypes.KVStoreService, authority sdk.AccAddress, evmKeeper types.EVMKeeper, clientKeeper types.ClientKeeper) Keeper {
 	// ensure gov module account is set and is not nil
 	if err := sdk.VerifyAddressFormat(authority); err != nil {
 		panic(err)
+	}
+	if evmKeeper == nil {
+		panic("evmKeeper cannot be nil")
+	}
+	if clientKeeper == nil {
+		panic("clientKeeper cannot be nil")
 	}
 
 	sb := collections.NewSchemaBuilder(storeService)
@@ -41,6 +48,7 @@ func NewKeeper(cdc codec.BinaryCodec, addressCodec address.Codec, storeService s
 		cdc:                   cdc,
 		addressCodec:          addressCodec,
 		authority:             authority,
+		evmKeeper:             evmKeeper,
 		clientKeeper:          clientKeeper,
 		ParamsItem:            collections.NewItem(sb, types.ParamsKey, "params", codec.CollValue[types.Params](cdc)),
 		ClientPrecompilesMap:  collections.NewMap(sb, types.ClientPrecompilesKey, "client_precompiles", collections.StringKey, codec.CollValue[types.ClientPrecompile](cdc)),
--- x/ibc/clients/types/expected_keepers.go
+++ x/ibc/clients/types/expected_keepers.go
@@ -1,7 +1,18 @@
 package types
 
 import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/tracing"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/evm/x/vm/statedb"
+	evmtypes "github.com/cosmos/evm/x/vm/types"
 
 	ibcexported "github.com/cosmos/ibc-go/v10/modules/core/exported"
 )
@@ -9,4 +20,22 @@ import (
 // ClientKeeper expected account IBC client keeper
 type ClientKeeper interface {
 	GetClientState(ctx sdk.Context, clientID string) (ibcexported.ClientState, bool)
+}
+
+// EVMKeeper defines the expected EVM keeper interface used on this module
+type EVMKeeper interface {
+	// TODO: remove unused methods
+	GetParams(ctx sdk.Context) evmtypes.Params
+	GetAccountWithoutBalance(ctx sdk.Context, addr common.Address) *statedb.Account
+	EstimateGasInternal(c context.Context, req *evmtypes.EthCallRequest, fromType evmtypes.CallType) (*evmtypes.EstimateGasResponse, error)
+	ApplyMessage(ctx sdk.Context, msg core.Message, tracer *tracing.Hooks, commit, internal bool) (*evmtypes.MsgEthereumTxResponse, error)
+	DeleteAccount(ctx sdk.Context, addr common.Address) error
+	IsAvailableStaticPrecompile(params *evmtypes.Params, address common.Address) bool
+	CallEVM(ctx sdk.Context, abi abi.ABI, from, contract common.Address, commit bool, gasCap *big.Int, method string, args ...interface{}) (*evmtypes.MsgEthereumTxResponse, error)
+	CallEVMWithData(ctx sdk.Context, from common.Address, contract *common.Address, data []byte, commit bool, gasCap *big.Int) (*evmtypes.MsgEthereumTxResponse, error)
+	GetCode(ctx sdk.Context, hash common.Hash) []byte
+	SetCode(ctx sdk.Context, hash []byte, bytecode []byte)
+	SetAccount(ctx sdk.Context, address common.Address, account statedb.Account) error
+	GetAccount(ctx sdk.Context, address common.Address) *statedb.Account
+	IsContract(ctx sdk.Context, address common.Address) bool
 }
