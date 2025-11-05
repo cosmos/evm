240b021f4db0e85718ace711164a53a443ee8bbc
Abdul Malek <me@almk.dev>
2025-11-05 11:30:52 -0500

    use nonce gap

--- tests/systemtests/mempool/test_ordering.go
+++ tests/systemtests/mempool/test_ordering.go
@@ -37,7 +37,7 @@ func RunTxsOrdering(t *testing.T, base *suite.BaseTestSuite) {
 						}
 
 						txInfo, err := s.SendTx(t, nodeId, signer.ID, nonceIdx, s.GasPriceMultiplier(10), big.NewInt(1))
-						if i == accIdx {
+						if nonceIdx <= uint64(accIdx) {
 							require.NoError(t, err, "failed to send tx")
 						} else {
 							require.Error(t, err, "expected error for nonce gap")
