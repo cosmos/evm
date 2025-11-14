1f3ece64b7a6ed1ba81758b7bdaffacb3f615071
yihuang <yi.codeplayer@gmail.com>
2025-11-14 11:41:32 +0800

    nixify evmd

---
+++ default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  stdenv,
+  buildGo125Module,
+  rev ? "dirty",
+}:
+buildGo125Module rec {
+  version = "v0.5.0";
+  pname = "evmd";
+  tags = [
+    "ledger"
+    "ledger_zemu"
+    "netgo" # pure go dns resolver
+    "osusergo" # pure go user/group lookup
+    "pebbledb"
+  ];
+  ldflags = [
+    "-X github.com/cosmos/cosmos-sdk/version.Name=evmd"
+    "-X github.com/cosmos/cosmos-sdk/version.AppName=${pname}"
+    "-X github.com/cosmos/cosmos-sdk/version.Version=${version}"
+    "-X github.com/cosmos/cosmos-sdk/version.BuildTags=${lib.concatStringsSep "," tags}"
+    "-X github.com/cosmos/cosmos-sdk/version.Commit=${rev}"
+  ];
+
+  src = lib.cleanSource ./.;
+  vendorHash = "sha256-+L4nKIKHV1bos9Trr50/kG69hR8iNL6MXLi9mun5iXQ=";
+  proxyVendor = true;
+  env = {
+    CGO_ENABLED = "1";
+  };
+
+  sourceRoot = "source/evmd";
+  subPackages = [ "cmd/evmd" ];
+
+  doCheck = false;
+  meta = with lib; {
+    description = "An EVM compatible framework for blockchain development with the Cosmos SDK";
+    homepage = "https://github.com/cosmos/evm";
+    license = licenses.asl20;
+    mainProgram = "evmd" + stdenv.hostPlatform.extensions.executable;
+    platforms = platforms.all;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      poetry2nix,
+    }:
+    let
+      rev = self.shortRev or "dirty";
+    in
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = self.overlays.default;
+          config = { };
+        };
+      in
+      {
+        legacyPackages = pkgs;
+        packages.default = pkgs.evmd;
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.evmd.go
+              pkgs.nixfmt-rfc-style
+              pkgs.solc
+            ];
+          };
+        };
+      }
+    ))
+    // {
+      overlays.default = [
+        poetry2nix.overlays.default
+        (final: super: {
+          evmd = final.callPackage ./. { inherit rev; };
+        })
+      ];
+    };
+}
