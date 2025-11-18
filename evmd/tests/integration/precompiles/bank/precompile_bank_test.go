428868aaa81dd70530368cc0293d42358d5f8cfe
Kyuhyeon Choi <bluejet1104@gmail.com>
2025-11-18 20:12:44 +0900

    test: fix bank precompile tests

--- evmd/tests/integration/precompiles/bank/precompile_bank_test.go
+++ evmd/tests/integration/precompiles/bank/precompile_bank_test.go
@@ -6,18 +6,30 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	evm "github.com/cosmos/evm"
-	"github.com/cosmos/evm/evmd/tests/integration"
-	"github.com/cosmos/evm/tests/integration/precompiles/bank"
+	"github.com/cosmos/evm/evmd/tests/appbuilder"
+	banktests "github.com/cosmos/evm/tests/integration/precompiles/bank"
 	testapp "github.com/cosmos/evm/testutil/app"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
 )
 
 func TestBankPrecompileTestSuite(t *testing.T) {
-	create := testapp.ToEvmAppCreator[evm.BankPrecompileApp](integration.CreateEvmd, "evm.BankPrecompileApp")
-	s := bank.NewPrecompileTestSuite(create)
+	create := testapp.ToEvmAppCreator[evm.BankPrecompileApp](
+		func(chainID string, evmChainID uint64, customBaseAppOptions ...func(*baseapp.BaseApp)) evm.EvmApp {
+			return appbuilder.CreateEvmdWithProfile(appbuilder.FullPrecompiles, chainID, evmChainID, customBaseAppOptions...)
+		},
+		"evm.BankPrecompileApp",
+	)
+	s := banktests.NewPrecompileTestSuite(create)
 	suite.Run(t, s)
 }
 
 func TestBankPrecompileIntegrationTestSuite(t *testing.T) {
-	create := testapp.ToEvmAppCreator[evm.BankPrecompileApp](integration.CreateEvmd, "evm.BankPrecompileApp")
-	bank.TestIntegrationSuite(t, create)
+	create := testapp.ToEvmAppCreator[evm.BankPrecompileApp](
+		func(chainID string, evmChainID uint64, customBaseAppOptions ...func(*baseapp.BaseApp)) evm.EvmApp {
+			return appbuilder.CreateEvmdWithProfile(appbuilder.FullPrecompiles, chainID, evmChainID, customBaseAppOptions...)
+		},
+		"evm.BankPrecompileApp",
+	)
+	banktests.TestIntegrationSuite(t, create)
 }
