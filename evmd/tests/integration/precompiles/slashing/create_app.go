93adab300ca3169ad5dbad5c7735c1061b673a5c
Kyuhyeon Choi <bluejet1104@gmail.com>
2025-11-19 20:37:39 +0900

    test: inject evmd to slashing precompile test

---
+++ evmd/tests/integration/precompiles/slashing/create_app.go
@@ -0,0 +1,75 @@
+package slashing
+
+import (
+	"os"
+
+	"github.com/cosmos/evm"
+	evmaddress "github.com/cosmos/evm/encoding/address"
+	eapp "github.com/cosmos/evm/evmd/app"
+	"github.com/cosmos/evm/evmd/tests/integration"
+	slashingprecompile "github.com/cosmos/evm/precompiles/slashing"
+
+	dbm "github.com/cosmos/cosmos-db"
+
+	"cosmossdk.io/log"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	distrkeeper "github.com/cosmos/cosmos-sdk/x/distribution/keeper"
+	slashingkeeper "github.com/cosmos/cosmos-sdk/x/slashing/keeper"
+)
+
+var _ evm.SlashingPrecompileApp = (*SlashingPrecompileApp)(nil)
+
+type SlashingPrecompileApp struct {
+	eapp.App
+}
+
+// CreateEvmd creates an evm app for regular integration tests (non-mempool)
+// This version uses a noop mempool to avoid state issues during transaction processing
+func CreateEvmd(chainID string, evmChainID uint64, customBaseAppOptions ...func(*baseapp.BaseApp)) evm.EvmApp {
+	// A temporary home directory is created and used to prevent race conditions
+	// related to home directory locks in chains that use the WASM module.
+	defaultNodeHome, err := os.MkdirTemp("", "evmd-temp-homedir")
+	if err != nil {
+		panic(err)
+	}
+
+	db := dbm.NewMemDB()
+	logger := log.NewNopLogger()
+	loadLatest := true
+	appOptions := integration.NewAppOptionsWithFlagHomeAndChainID(defaultNodeHome, evmChainID)
+	baseAppOptions := append(customBaseAppOptions, baseapp.SetChainID(chainID))
+
+	eapp := eapp.New(
+		logger,
+		db,
+		nil,
+		loadLatest,
+		appOptions,
+		baseAppOptions...,
+	)
+
+	app := &SlashingPrecompileApp{
+		App: *eapp,
+	}
+
+	slashingPrecompile := slashingprecompile.NewPrecompile(
+		app.GetSlashingKeeper(),
+		slashingkeeper.NewMsgServerImpl(app.GetSlashingKeeper()),
+		app.GetBankKeeper(),
+		evmaddress.NewEvmCodec(sdktypes.GetConfig().GetBech32ValidatorAddrPrefix()),
+		evmaddress.NewEvmCodec(sdktypes.GetConfig().GetBech32ConsensusAddrPrefix()),
+	)
+	app.App.GetEVMKeeper().RegisterStaticPrecompile(slashingPrecompile.Address(), slashingPrecompile)
+
+	return app
+}
+
+func (app *SlashingPrecompileApp) GetSlashingKeeper() slashingkeeper.Keeper {
+	return app.SlashingKeeper
+}
+
+func (app *SlashingPrecompileApp) GetDistrKeeper() distrkeeper.Keeper {
+	return app.DistributionKeeper
+}
--- evmd/tests/integration/precompiles/slashing/precompile_slashing_test.go
+++ evmd/tests/integration/precompiles/slashing/precompile_slashing_test.go
@@ -6,18 +6,17 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	evm "github.com/cosmos/evm"
-	"github.com/cosmos/evm/evmd/tests/integration"
 	"github.com/cosmos/evm/tests/integration/precompiles/slashing"
 	testapp "github.com/cosmos/evm/testutil/app"
 )
 
 func TestSlashingPrecompileTestSuite(t *testing.T) {
-	create := testapp.ToEvmAppCreator[evm.SlashingPrecompileApp](integration.CreateEvmd, "evm.SlashingPrecompileApp")
+	create := testapp.ToEvmAppCreator[evm.SlashingPrecompileApp](CreateEvmd, "evm.SlashingPrecompileApp")
 	s := slashing.NewPrecompileTestSuite(create)
 	suite.Run(t, s)
 }
 
 func TestStakingPrecompileIntegrationTestSuite(t *testing.T) {
-	create := testapp.ToEvmAppCreator[evm.SlashingPrecompileApp](integration.CreateEvmd, "evm.SlashingPrecompileApp")
+	create := testapp.ToEvmAppCreator[evm.SlashingPrecompileApp](CreateEvmd, "evm.SlashingPrecompileApp")
 	slashing.TestPrecompileIntegrationTestSuite(t, create)
 }
