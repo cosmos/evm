86f2f4c50cc627caa68f05a62e4feda8173013e1
Kyuhyeon Choi <bluejet1104@gmail.com>
2025-11-20 15:50:49 +0900

    chore: remove unnecessary app wrapping for distribution precompile

--- evmd/tests/integration/precompiles/distribution/precompile_distribution_test.go
+++ evmd/tests/integration/precompiles/distribution/precompile_distribution_test.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/stretchr/testify/suite"
 
+	"github.com/cosmos/evm/evmd/tests/integration"
 	"github.com/cosmos/evm/tests/integration/precompiles/distribution"
 )
 
 func TestDistributionPrecompileTestSuite(t *testing.T) {
-	s := distribution.NewPrecompileTestSuite(CreateEvmd)
+	s := distribution.NewPrecompileTestSuite(integration.CreateEvmd)
 	suite.Run(t, s)
 }
 
 func TestDistributionPrecompileIntegrationTestSuite(t *testing.T) {
-	distribution.TestPrecompileIntegrationTestSuite(t, CreateEvmd)
+	distribution.TestPrecompileIntegrationTestSuite(t, integration.CreateEvmd)
 }
