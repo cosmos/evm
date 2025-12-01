cd99a34a0b20e2b1271d72f17a6c83fdf01d09d6
Kyuhyeon Choi <bluejet1104@gmail.com>
2025-12-01 16:26:04 +0900

    test: add ics20 precompile test

---
+++ evmd/tests/integration/precompiles/ics20/precompile_ics20_test.go
@@ -0,0 +1,23 @@
+package ics20
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/cosmos/evm"
+	"github.com/cosmos/evm/evmd/tests/ibc"
+	"github.com/cosmos/evm/tests/integration/precompiles/ics20"
+	testapp "github.com/cosmos/evm/testutil/app"
+)
+
+func TestICS20PrecompileTestSuite(t *testing.T) {
+	create := testapp.ToIBCAppCreator[evm.ICS20PrecompileApp](ibc.SetupEvmd, "evm.ICS20PrecompileApp")
+	s := ics20.NewPrecompileTestSuite(t, create)
+	suite.Run(t, s)
+}
+
+func TestICS20PrecompileIntegrationTestSuite(t *testing.T) {
+	create := testapp.ToIBCAppCreator[evm.ICS20PrecompileApp](ibc.SetupEvmd, "evm.ICS20PrecompileApp")
+	ics20.TestPrecompileIntegrationTestSuite(t, create)
+}
