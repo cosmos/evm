70ee89c3950c191f93b49679824de14c139b15c1
Matt Acciai <matt@cosmoslabs.io>
2025-12-13 12:27:11 -0500

    remove prints

--- mempool/txpool/legacypool/legacypool.go
+++ mempool/txpool/legacypool/legacypool.go
@@ -20,7 +20,6 @@ package legacypool
 import (
 	"context"
 	"errors"
-	"fmt"
 	"maps"
 	"math/big"
 	"slices"
@@ -2151,7 +2150,6 @@ func (pool *LegacyPool) HasPendingAuth(addr common.Address) bool {
 // markTxPromoted calls the OnTxPromoted callback if it has been supplied.
 func (pool *LegacyPool) markTxPromoted(tx *types.Transaction) {
 	if pool.OnTxPromoted != nil {
-		fmt.Println("TX PROMOTED")
 		pool.OnTxPromoted(tx)
 	}
 }
@@ -2159,7 +2157,6 @@ func (pool *LegacyPool) markTxPromoted(tx *types.Transaction) {
 // markTxRemoved calls the OnTxRemoved callback if it has been supplied.
 func (pool *LegacyPool) markTxRemoved(tx *types.Transaction, p PoolType) {
 	if pool.OnTxRemoved != nil {
-		fmt.Println("TX REMOVED")
 		pool.OnTxRemoved(tx, p)
 	}
 }
@@ -2167,7 +2164,6 @@ func (pool *LegacyPool) markTxRemoved(tx *types.Transaction, p PoolType) {
 // markTxEnqueued calls the OnTxEnqueued callback if it has been supplied.
 func (pool *LegacyPool) markTxEnqueued(tx *types.Transaction) {
 	if pool.OnTxEnqueued != nil {
-		fmt.Println("TX ENQUEUED")
 		pool.OnTxEnqueued(tx)
 	}
 }
