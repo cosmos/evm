75dcd8c8abde399e56748551edecb6afa3ef970e
Vlad <vlad@cosmoslabs.io>
2026-01-20 13:21:06 +0100

    10s blocktime

--- tests/systemtests/mempool/test_broadcast.go
+++ tests/systemtests/mempool/test_broadcast.go
@@ -371,7 +371,7 @@ func RunTxBroadcasting(t *testing.T) {
 
 	// Now modify the consensus timeout to slow down block production
 	// This gives us time to verify broadcasting happens before blocks are committed
-	s.ModifyConsensusTimeout(t, "20s")
+	s.ModifyConsensusTimeout(t, "10s")
 
 	for _, to := range testOptions {
 		s.SetOptions(to)
