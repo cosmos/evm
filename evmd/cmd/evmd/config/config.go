76e67dcd799ec7c83368edefa96a79d2a5d2c5a2
Vlad <vlad@cosmoslabs.io>
2026-03-10 14:20:06 -0400

    hardcoded astake

--- evmd/cmd/evmd/config/config.go
+++ evmd/cmd/evmd/config/config.go
@@ -26,9 +26,9 @@ var ChainsCoinInfo = map[uint64]evmtypes.EvmCoinInfo{
 	},
 	// EVMChainID provides a chain ID used for internal testing
 	EVMChainID: {
-		Denom:         "atest",
-		ExtendedDenom: "atest",
-		DisplayDenom:  "test",
+		Denom:         "astake",
+		ExtendedDenom: "astake",
+		DisplayDenom:  "stake",
 		Decimals:      evmtypes.EighteenDecimals,
 	},
 }
@@ -49,9 +49,9 @@ const (
 	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key.
 	Bech32PrefixConsPub = Bech32Prefix + sdk.PrefixValidator + sdk.PrefixConsensus + sdk.PrefixPublic
 	// DisplayDenom defines the denomination displayed to users in client applications.
-	DisplayDenom = "atom"
+	DisplayDenom = "stake"
 	// BaseDenom defines to the default denomination used in the Cosmos EVM example chain.
-	BaseDenom = "aatom"
+	BaseDenom = "astake"
 	// BaseDenomUnit defines the precision of the base denomination.
 	BaseDenomUnit = 18
 	// EVMChainID defines the EIP-155 replay-protection chain id for the current ethereum chain config.
--- evmd/cmd/evmd/config/constants.go
+++ evmd/cmd/evmd/config/constants.go
@@ -2,10 +2,10 @@ package config
 
 const (
 	// ExampleChainDenom is the denomination of the Cosmos EVM example chain's base coin.
-	ExampleChainDenom = "aatom"
+	ExampleChainDenom = "astake"
 
 	// ExampleDisplayDenom is the display denomination of the Cosmos EVM example chain's base coin.
-	ExampleDisplayDenom = "atom"
+	ExampleDisplayDenom = "stake"
 
 	// EighteenDecimalsChainID is the chain ID for the 18 decimals chain.
 	EighteenDecimalsChainID = 9001
