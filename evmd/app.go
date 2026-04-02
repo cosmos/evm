76cc259fc54a6c9320c9c02236dc783ede061ed1
mmsqe <tqd0800210105@gmail.com>
2026-04-02 21:38:30 +0800

    fix build

--- evmd/app.go
+++ evmd/app.go
@@ -150,6 +150,7 @@ var (
 
 type customRunner struct {
 	*txnrunner.STMRunner
+	txDecoder sdk.TxDecoder
 }
 
 func (r *customRunner) Run(ctx context.Context, ms storetypes.MultiStore, txs [][]byte, deliverTx sdk.DeliverTxFunc) ([]*abci.ExecTxResult, error) {
@@ -158,7 +159,7 @@ func (r *customRunner) Run(ctx context.Context, ms storetypes.MultiStore, txs []
 		return nil, err
 	}
 
-	return evmtypes.PatchTxResponses(results), nil
+	return evmtypes.PatchTxResponses(results, txs, r.txDecoder), nil
 }
 
 // EVMD extends an ABCI application, but with most of its parameters exported.
@@ -797,6 +798,7 @@ func NewExampleApp(
 				return app.EVMKeeper.GetParams(sdk.NewContext(ms, cmtproto.Header{}, false, log.NewNopLogger())).EvmDenom
 			},
 		),
+		txDecoder: txDecoder,
 	})
 
 	return app
