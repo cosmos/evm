86fd93d84531dad71d996781f2e07155ce308230
Vlad J <vladjdk@gmail.com>
2026-04-22 17:42:28 -0400

    chore(tests): fix ibc tests failing with different balances (#1124)
    * chore(tests): fix ibc tests failing with different balances
    
    * fix stale contexts

--- evmd/tests/ibc/v2_transfer_test.go
+++ evmd/tests/ibc/v2_transfer_test.go
@@ -82,7 +82,6 @@ func (suite *TransferTestSuiteV2) SetupTest() {
 }
 
 func TestTransferTestSuiteV2(t *testing.T) {
-	t.Skip("STACK-2601: fix IBC tests")
 	testifysuite.Run(t, new(TransferTestSuiteV2))
 }
 
