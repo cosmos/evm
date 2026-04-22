c6e6445816d04b43e8417c62d3715fd0e9bbe28c
Eric Warehime <eric.warehime@gmail.com>
2026-04-22 12:11:42 -0500

    Fix lint

--- x/vm/types/tx_args.go
+++ x/vm/types/tx_args.go
@@ -140,7 +140,7 @@ func (args *TransactionArgs) CallDefaults(globalGasCap uint64, baseFee *big.Int,
 // core evm. This method is used in calls and traces that do not require a real
 // live transaction.
 // Assumes that fields are not nil, i.e. setDefaults or CallDefaults has been called.
-func (args *TransactionArgs) ToMessage(baseFee *big.Int, skipNonceCheck, SkipTransactionChecks bool) *core.Message {
+func (args *TransactionArgs) ToMessage(baseFee *big.Int, skipNonceCheck, skipTransactionChecks bool) *core.Message {
 	var (
 		gasPrice  *big.Int
 		gasFeeCap *big.Int
@@ -188,7 +188,7 @@ func (args *TransactionArgs) ToMessage(baseFee *big.Int, skipNonceCheck, SkipTra
 		BlobHashes:            args.BlobHashes,
 		SetCodeAuthorizations: args.AuthorizationList,
 		SkipNonceChecks:       skipNonceCheck,
-		SkipTransactionChecks: SkipTransactionChecks,
+		SkipTransactionChecks: skipTransactionChecks,
 	}
 }
 
