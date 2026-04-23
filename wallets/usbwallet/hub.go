6543f5e6818205374d8dbbd13fc3c9a526548b70
Puneet <59960662+puneet2019@users.noreply.github.com>
2026-04-23 19:10:39 +0400

    chore: add Ledger Nano Gen5 USB product IDs (#1095)

--- wallets/usbwallet/hub.go
+++ wallets/usbwallet/hub.go
@@ -64,6 +64,7 @@ func NewLedgerHub() (*Hub, error) {
 		0x0005, /* Ledger Nano S Plus */
 		0x0006, /* Ledger Nano FTS */
 		0x0007, /* Ledger Flex */
+		0x0008, /* Ledger Nano Gen5 */
 
 		0x0000, /* WebUSB Ledger Blue */
 		0x1000, /* WebUSB Ledger Nano S */
@@ -71,6 +72,7 @@ func NewLedgerHub() (*Hub, error) {
 		0x5000, /* WebUSB Ledger Nano S Plus */
 		0x6000, /* WebUSB Ledger Nano FTS */
 		0x7000, /* WebUSB Ledger Flex */
+		0x8000, /* WebUSB Ledger Nano Gen5 */
 	}, 0xffa0, 0, newLedgerDriver)
 }
 
