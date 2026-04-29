3054050e245b212b6bdcc07677e1ff404492c7eb
Matt Acciai <matt@cosmoslabs.io>
2026-04-29 16:11:38 -0400

    remove `NotifiedMempool` interface

--- mempool/interface.go
+++ mempool/interface.go
@@ -12,17 +12,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// NotifiedMempool is the set of methods that a mempool must implement in order
-// to be notified of new blocks by this Keeper via the EndBlocker.
-type NotifiedMempool interface {
-	// HasEventBus returns true if the mempool has an event bus configured to
-	// get cometbft events
-	HasEventBus() bool
-
-	// GetBlockchain returns the mempools blockchain representation.
-	GetBlockchain() *Blockchain
-}
-
 type VMKeeperI interface {
 	GetBaseFee(ctx sdk.Context) *big.Int
 	GetParams(ctx sdk.Context) (params vmtypes.Params)
