008c171178abc8456399e4fc31fe6994ab969da0
mattac21 <matt@cosmoslabs.io>
2026-05-20 13:36:15 -0400

    fix(statedb): snapshot locked balance on statedb account  (#1187)
    * snapshot locked balance on statedb account creation to calculate spendable + locked bank balance after locked balance changes via precompile
    
    * lint formatting
    
    * modify x/erc20 registering code hash to not silently drop existing accounts locked balacne
    
    * update comments
    
    * Update x/vm/keeper/statedb.go
    
    Co-authored-by: Vlad J <vladjdk@gmail.com>
    
    ---------
    
    Co-authored-by: Vlad J <vladjdk@gmail.com>

--- x/vm/keeper/keeper.go
+++ x/vm/keeper/keeper.go
@@ -337,6 +337,18 @@ func (k *Keeper) SpendableCoin(ctx sdk.Context, addr common.Address) *uint256.In
 	return result
 }
 
+// lockedCoin loads account's locked balance of the gas token.
+func (k *Keeper) lockedCoin(ctx sdk.Context, addr common.Address) *big.Int {
+	ctx, span := ctx.StartSpan(tracer, "LockedCoin", trace.WithAttributes(attribute.String("address", addr.Hex())))
+	defer span.End()
+	cosmosAddr := sdk.AccAddress(addr.Bytes())
+
+	lockedCoins := k.bankWrapper.LockedCoins(ctx, cosmosAddr)
+	lockedGasCoin := lockedCoins.AmountOf(types.GetEVMCoinDenom())
+
+	return lockedGasCoin.BigInt()
+}
+
 // GetBalance load account's balance of gas token.
 func (k *Keeper) GetBalance(ctx sdk.Context, addr common.Address) *uint256.Int {
 	ctx, span := ctx.StartSpan(tracer, "GetBalance", trace.WithAttributes(attribute.String("address", addr.Hex())))
